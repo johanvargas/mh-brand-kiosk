f24cf700475c62fc707bb1a4f1e0c9e7791c6ef0
johanvargas <savoyparkr@gmail.com>
2026-01-19 19:02:39 -0600

    socket io working

--- backend/websocketServer.js
+++ backend/websocketServer.js
@@ -28,33 +28,29 @@ app.use(express.static(dirname + "/"));
 io.on("connection", (socket) => {
 	console.log("socket id: ", socket.id);
   socket.emit("hello", socket.id);
-
   socket.on("trigger", (data) => {
     console.log("trigger number; ", data)
+    setTimeout(() => sendCommand(`G005B[A ${data} 100 12]`), 50);
   })
 });
 
 /* SERIAL PORT SERVER */
 import { ReadlineParser } from "@serialport/parser-readline";
 import { SerialPort } from "serialport";
-import { MockBinding } from "@serialport/binding-mock";
+//import { MockBinding } from "@serialport/binding-mock";
 
-MockBinding.createPort('/dev/null', { echo: true, record: true })
-const serial_port = new SerialPort({ binding: MockBinding, path: "/dev/null",baudRate: 115200 });
-
+//MockBinding.createPort('/dev/null', { echo: true, record: true })
+//const serial_port = new SerialPort({ binding: MockBinding, path: "/dev/ttyUSB0",baudRate: 115200 });
+const serial_port = new SerialPort({ path: "/dev/ttyUSB0", baudRate: 115200 });
 console.log(serial_port);
 
 serial_port.on("open", (socket) => {
 	console.log("Serial port - mh-kiosk - opened. Listening for Nexmosphere data...");
 });
 
-
 serial_port.on("connection", (socket) => {
 	console.log("Serial port - mh-kiosk - connected");
-
 });
-
-
 const parser = serial_port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
 
 function sendCommand(command) {
@@ -67,10 +63,12 @@ function sendCommand(command) {
 	});
 }
 
-setTimeout(() => sendCommand("G005B[B 3 100 12]"), 50);
+setTimeout(() => sendCommand("G005B[A 1 100 12]"), 50);
 
 // Receive msgs from client
 parser.on("data", (data) => {
+
+	console.log("data received from serial dev: ", data)
 	//if (data === "X007B[ZONE01=EXIT]") {
 	//	console.log("Signal Received:", data);
 	//	io.emit("serialdata", { data: data, time: Date.now(), point: 10 });
